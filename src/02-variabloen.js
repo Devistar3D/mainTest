544051bad19bf7f784f7479034e5572c7af1432f
Devistar3 / Kai <144699592+devistar3@users.noreply.github.com>
2024-01-09 15:08:42 +0000

    änderungen

---
+++ src/02-variabloen.js
@@ -0,0 +1,63 @@
+
+                                          // Zeilenkommentar
+
+/*
+    Block-Kommentar
+*/
+
+/***** Variablen *****/
+
+// console.log("Hi!");                                        // Ausgabe
+// console.log(Hi);                                           // Ausgabe
+
+/* Deklaration + Wertzuweisung I */
+
+// let firstName;                                                // Deklaration (Definition)
+// console.log(firstName);                                       // Ausgabe
+// firstName = "Max";
+
+// console.log(firstName);                                       // Ausgabe
+
+// let familyName = "Mütze";                                     // Deklaration + Wertzuweisung
+
+// console.log("Hi, ich bin " + firstName + " " + familyName);
+
+// console.log("-------------");
+// console.log("-------------");
+// console.log("-------------");
+
+/*
+    Wertzuweisung II + Datentypen
+    JS ist ein untypisierte Sprache!
+*/
+
+// let test;                                                       // Deklaration
+// console.log("Datentyp: " + typeof test);
+// console.log("Inhalt: " + test);
+// console.log("-------------");
+// test = "hello";                                                 // string
+// console.log("Datentyp: " + typeof test);
+// console.log("Inhalt: " + test);
+// console.log("-------------");
+// test = 2;                                                        // number
+// console.log("Datentyp: " + typeof test);
+// console.log("Inhalt: " + test);
+// console.log("-------------");
+// test = true;                                                     // 
+// console.log("Datentyp: " + typeof test);
+// console.log("Inhalt: " + test);
+// console.log("-------------");
+// test = true;  
+
+
+
+
+let test;                                                        // Deklaration
+test = "hi";                                                     // Wertzuweisung (Initialisierung)
+test = "hello"; 
+console.log("Inhalt; " + test);                                  // Ausgabe
+
+const test2 = "Hello!";                                          // Deklaration + Wertzuweisung
+// test2 = "Hello!";
+console.log("Inhalt: " + test2);                                 // Ausgabe
+
